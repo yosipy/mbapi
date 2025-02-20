0958bd82df1902406cfca280321c62ff76bcaa84
yosipy <todacchi.desu@gmail.com>
2025-02-20 20:21:54 +0900

    add project & post tables
    npx prisma migrate diff \
      --from-empty \
      --to-schema-datamodel ./prisma/schema.prisma \
      --script \                                          --output migrations/0001_create_init_tables.sql

--- migrations/0001_create_init_tables.sql
+++ migrations/0001_create_init_tables.sql
@@ -1,1 +1,23 @@
--- Migration number: 0001 	 2025-02-19T15:52:30.814Z
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "name" TEXT NOT NULL,
+    "icon_path" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "nano_id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    CONSTRAINT "Post_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_nano_id_key" ON "Post"("nano_id");
