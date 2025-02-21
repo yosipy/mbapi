d2f5125f37642774e0154368e5264a8a0b6b6ef4
yosipy <todacchi.desu@gmail.com>
2025-02-21 20:09:45 +0900

    add api: post api/tokens

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Hono } from "hono";
 
+import apiTokens from "./routes/api/tokens";
+
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.route("/api/tokens", apiTokens);
 
 export default app;
