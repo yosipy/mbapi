11169fb82e6515391c3ae8c6dabb8a69bc01e914
yosipy <todacchi.desu@gmail.com>
2025-03-27 22:45:50 +0900

    /api/tokensを削除

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { Hono } from "hono";
 
-import apiTokens from "./routes/api/tokens";
-
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
-
-app.route("/api/tokens", apiTokens);
 
 export default app;
